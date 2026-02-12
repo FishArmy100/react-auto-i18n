71454a85cc341a293455a876668cad87c164e24c
FishArmy100 <87653948+FishArmy100@users.noreply.github.com>
2026-02-11 23:50:50 -0500

    translations working

--- packages/auto-i18n-cli/src/index.ts
+++ packages/auto-i18n-cli/src/index.ts
@@ -1,13 +1,26 @@
 #!/usr/bin/env node
 
-import process from "node:process";
-import { run_python } from "./python.js";
+import { run_python, TranslateArgs } from "./python.js";
+import * as fs from 'fs';
+import * as path from 'path';
 
 function main() 
 {
-    const args = process.argv.slice(2);
+    const args: TranslateArgs = {
+		langs: ["fra_Latn", "spa_Latn"],
+		src_lang: "eng_Latn",
+		segments: {
+			"test": "This is some test text",
+			"other.test": "This is more test text",
+		}
+	};
+	
     run_python(args)
-		.then(console.log)
+		.then(o => {
+			const out_path = "out/output.txt"
+			fs.mkdirSync(path.dirname(out_path), { recursive: true });
+			fs.writeFileSync(out_path, JSON.stringify(o, null, 2));
+		})
 		.catch(console.error);
 }
 
---
+++ translator/translate.py
@@ -0,0 +1,58 @@
+import sys
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, NllbTokenizer
+from pydantic import BaseModel
+import json
+import io
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+class Args(BaseModel):
+    src_lang: str
+    segments: dict[str, str]
+    langs: list[str]
+
+class Ret(BaseModel):
+    values: dict[str, dict[str, str]]
+
+def translate(segment: str, lang: str, src_lang, model, tokenizer) -> str:
+    tokenizer.src_lang = src_lang
+    inputs = tokenizer(segment, return_tensors="pt")
+    
+    # Get the token ID for the target language
+    lang_id = tokenizer.convert_tokens_to_ids(lang)
+
+    translated_tokens = model.generate(
+        **inputs,
+        forced_bos_token_id=lang_id,
+        max_length=100
+    )
+
+    translation = tokenizer.batch_decode(translated_tokens, skip_special_tokens=True)[0]
+    return translation
+
+def main():
+    try:
+        args = Args.model_validate_json(sys.argv[1])
+
+        model_name = "facebook/nllb-200-distilled-600M"
+        tokenizer = NllbTokenizer.from_pretrained(model_name)
+        model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+
+        ret = Ret(values={})
+        for l in args.langs:
+            ret.values[l] = {}
+            for k, s in args.segments.items():
+                translated = translate(s, l, args.src_lang, model, tokenizer)
+                ret.values[l][k] = translated
+
+        ret.values[args.src_lang] = {}
+        for k, s in args.segments.items():
+            ret.values[args.src_lang][k] = s
+        
+        print(json.dumps(ret.model_dump(), ensure_ascii=False))
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
