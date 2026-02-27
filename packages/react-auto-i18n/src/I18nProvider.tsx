18294b81c9697bc64c454bf80326974ac208bc38
FishArmy100 <87653948+FishArmy100@users.noreply.github.com>
2026-02-27 17:59:13 -0500

    Update I18nProvider.tsx

--- packages/react-auto-i18n/src/I18nProvider.tsx
+++ packages/react-auto-i18n/src/I18nProvider.tsx
@@ -2,23 +2,85 @@ import { I18nDatabase, setCurrentLocalRaw, setI18nDatabaseRaw } from "./i18n";
 import { LangScriptCode } from "./langs";
 import React, { createContext, useContext, useEffect, useState } from "react"
 
+/**
+ * The context type for the `I18nProvider`
+ */
 export interface I18nContextType
 {
+    /**
+     * The currently set `LangScriptCode`
+     */
     readonly locale: LangScriptCode,
+    /**
+     * The currently set `I18nDatabase`
+     */
     readonly database: I18nDatabase,
+
+    /**
+     * Sets the current `LangScriptCode`
+     * @param locale The current local to set
+     */
     readonly setLocale: (locale: LangScriptCode) => void,
+    
+    /**
+     * Sets the current `I18nDatabase`
+     * @param locale The current local to set
+     */
     readonly setDatabase: (database: I18nDatabase) => void,
+
+    /**
+     * This is essentially a helper function for doing Object.keys on the database
+     * @returns Returns all the loaded `LangScriptCode`s in the current I18nDatabase.
+     */
     readonly getLocales: () => LangScriptCode[],
 }
 
 const I18nContext = createContext<I18nContextType | null>(null);
 
+/**
+ * The properties for the `I18nProvider`
+ */
 export type I18nProviderProps = {
+    /**
+     * The providers children
+     */
     children: React.ReactNode,
+    /**
+     * The default language code. When passed to the `I18nProvider`, defaults to `"eng_Latn"`
+     */
     default_lang: LangScriptCode,
+
+    
+    /**
+     * The default database. When passed to the `I18nProvider`, defaults to `{}`
+     */
     default_database: I18nDatabase,
 }
 
+/**
+ * The provider for the `I18nDatabase` and current local.
+ * 
+ * Allows for getting and setting the global state used by `__t` in a more React like way.
+ * ### **Example:**
+ * ```tsx
+ * import { StrictMode } from 'react'
+ * import { createRoot } from 'react-dom/client'
+ * import App from './App.tsx'
+ * import { I18nProvider } from "react-auto-i18n";
+ * import db from "./assets/translations.json";
+ * 
+ * createRoot(document.getElementById('root')!).render(
+ * 	  <I18nProvider 
+ * 	  	default_lang="eng_Latn" 
+ * 	  	default_database={db}
+ * 	  >
+ * 	  	<App />
+ * 	  </I18nProvider>
+ * )
+ * ```
+ * 
+ * @param props The `I18nProviderProps` for this provider
+ */
 export function I18nProvider({
     children,
     default_lang = "eng_Latn",
@@ -54,6 +116,19 @@ export function I18nProvider({
     )
 }
 
+/**
+ * Allows for the modification and usage of the global database and locale states. Must be used on the context of a `I18nProvider`.
+ * 
+ * ### **Example:**
+ * ```ts
+ * const i18n = useI18n();
+ * i18n.setLocale("spa_Latn");
+ * 
+ * let msg = __t("message", "Hello!");
+ * console.log(msg); // Hola!
+ * ```
+ * @returns The instance of the current `I18nContext`
+ */
 export function useI18n(): I18nContextType
 {
     const ctx = useContext(I18nContext);
